ed2e71a6c5b28c4d1825c4ab2a5bf8c05158c32b
Adam Hartz <ahartz1@gmail.com>
2015-10-06 16:10:30 -0400

    Add self.id to models

--- movieratings/lensview/admin.py
+++ movieratings/lensview/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Rater, Movie, Rating
+
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['user']
 
 # Register your models here.
+admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(Rating)
--- movieratings/lensview/models.py
+++ movieratings/lensview/models.py
@@ -4,14 +4,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Rater(models.Model):
-    user = models.ForeignKey(User)
+    age = models.IntegerField()
 
+    def __str__(self):
+        return 'id={}, age={}'.format(self.id, self.age)
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
 
     def __str__(self):
-        return '{}'.format(self.title)
+        return 'id={}, title={}'.format(self.id, self.title)
 
 class Rating(models.Model):
     user = models.ForeignKey(User)
@@ -19,6 +21,6 @@ class Rating(models.Model):
     stars = models.IntegerField()
 
     def __str__(self):
-        return '{}, {}: {}'.format(self.user,
-                                   self.stars,
-                                   self.title)
+        return 'user={}, stars={}, title={}'.format(self.user,
+                                                    self.stars,
+                                                    self.title)
