a373368d71d7205084ccf6dcffc15eec3e47d72b
Adam Hartz <ahartz1@gmail.com>
2015-10-06 15:46:58 -0400

    Add Rater, Movie, and Rating class defs

--- movieratings/lensview/models.py
+++ movieratings/lensview/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Rater(models.Model):
+    user = models.ForeignKey(User)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User)
+    title = models.ForeignKey(Movie)
+    stars = models.IntegerField()
+
+    def __str__(self):
+        return '{}, {}: {}'.format(self.user,
+                                   self.stars,
+                                   self.title)
