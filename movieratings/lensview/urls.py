5393d9a350da74e5f8d819aaf11d86c464637047
Adam Hartz <ahartz1@gmail.com>
2015-10-08 10:33:28 -0400

    Changed top20 to homepage

--- movieratings/lensview/urls.py
+++ movieratings/lensview/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^movies/top_20$', views.top_20),
+    url(r'$', views.top_20),
     url(r'^movies/(?P<movie_id>\d+)$', views.show_movie, name='movie_detail'),
     url(r'^raters/(?P<rater_id>\d+)$', views.show_rater, name='rater_detail'),
 ]
