152ac8d37486653d9b74ee0a20b667476914cc1d
Adam Hartz <ahartz1@gmail.com>
2015-10-09 11:43:10 -0400

    Add Rater, Movie, and Rating models

--- movieratings/lensview/models.py
+++ movieratings/lensview/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Rater(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+
+    GENDER = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+
+    gender = models.CharField(max_length=1, choices=GENDER)
+    age = models.SmallIntegerField()
+    occupation = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=5)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.pk
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genres = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.pk
+
+
+class Rating(models.Model):
+    rater = Rater()
+    movie = Movie()
+    stars = models.SmallIntegerField()
+
+    def __str__(self):
+        return '@{}: {}\u2505 -> {}'.format(
+            self.rater, self.stars, self.movie.title)
+
+
+#
+
+#
+
+#
