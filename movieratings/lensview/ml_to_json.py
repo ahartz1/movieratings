8732b32b93d8cd040beb049faae58dd1cbd620a0
Adam Hartz <ahartz1@gmail.com>
2015-10-09 14:51:10 -0400

    Create file to convert ml to json

--- movieratings/lensview/ml_to_json.py
+++ movieratings/lensview/ml_to_json.py
@@ -1,46 +1,3 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-# Create your models here.
-
-
-class Rater(models.Model):
-    MALE = 'M'
-    FEMALE = 'F'
-
-    GENDER = (
-        (MALE, 'Male'),
-        (FEMALE, 'Female')
-    )
-
-    gender = models.CharField(max_length=1, choices=GENDER)
-    age = models.SmallIntegerField()
-    occupation = models.CharField(max_length=255)
-    zipcode = models.CharField(max_length=5)
-    user = models.OneToOneField(User, null=True)
-
-    def __str__(self):
-        return self.pk
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    genres = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.pk
-
-
-class Rating(models.Model):
-    rater = models.ForeignKey(Rater)
-    movie = models.ForeignKey(Movie)
-    stars = models.SmallIntegerField()
-
-    def __str__(self):
-        return '@{}: {}\u2505 -> {}'.format(
-            self.rater, self.stars, self.movie.title)
-
-
 def ml_users_to_json():
     import csv
     import json
@@ -49,7 +6,7 @@ def ml_users_to_json():
     # Then create a json from it
     user_data = []
 
-    with open('ml-1m/users.dat') as f:
+    with open('../ml-1m/users.dat') as f:
         reader = csv.DictReader([line.replace('::', '\t') for line in f],
                                 fieldnames=['UserID',
                                             'Age',
@@ -70,7 +27,7 @@ def ml_users_to_json():
             }
             user_data.append(user)
 
-    with open('lensview/fixtures/users.json', 'w') as f:
+    with open('fixtures/users.json', 'w') as f:
         f.write(json.dumps(user_data))
 
 
@@ -82,7 +39,7 @@ def ml_movies_to_json():
     # Then create a json from it
     movie_data = []
 
-    with open('ml-1m/movies.dat') as f:
+    with open('../ml-1m/movies.dat', encoding='windows-1252') as f:
         reader = csv.DictReader([line.replace('::', '\t') for line in f],
                                 fieldnames=['MovieID',
                                             'Title',
@@ -100,7 +57,7 @@ def ml_movies_to_json():
             }
             movie_data.append(movie)
 
-    with open('lensview/fixtures/movies.json', 'w') as f:
+    with open('fixtures/movies.json', 'w') as f:
         f.write(json.dumps(movie_data))
 
 
@@ -112,7 +69,7 @@ def ml_ratings_to_json():
     # Then create a json from it
     rating_data = []
 
-    with open('ml-1m/ratings.dat') as f:
+    with open('../ml-1m/ratings.dat') as f:
         reader = csv.DictReader([line.replace('::', '\t') for line in f],
                                 fieldnames=['UserID',
                                             'MovieID',
@@ -131,10 +88,10 @@ def ml_ratings_to_json():
             }
             rating_data.append(rating)
 
-    with open('lensview/fixtures/ratings.json', 'w') as f:
+    with open('fixtures/ratings.json', 'w') as f:
         f.write(json.dumps(rating_data))
 
-def ml_all_to_json():
-    ml_users_to_json()
-    ml_movies_to_json()
-    ml_ratings_to_json()
+
+ml_users_to_json()
+ml_movies_to_json()
+ml_ratings_to_json()
